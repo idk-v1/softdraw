1c00dbf17c3eddaa9fd6082552b27a1f6a9d24d9
idk-v1 <benhamilton.jpg@gmail.com>
2024-10-31 13:23:40 -0500

    Keyboard bounds checking

--- Examples/Sneak/Sneak.c
+++ Examples/Sneak/Sneak.c
@@ -23,10 +23,12 @@ int main()
     sft_screenshot(screen);
 
     sft_window* window = sft_window_open("softdraw", 800, 500, -1, -1,
-        sft_flag_darkmode | sft_flag_fullscreen | sft_flag_topmost | sft_flag_passthru | sft_flag_syshide);
+        sft_flag_darkmode | sft_flag_fullscreen | sft_flag_topmost | sft_flag_passthru | sft_flag_syshide | sft_flag_hidden);
     if (!window)
         return 1;
     window->fpsLimit = 30;
+
+    bool hidden = true;
 
     while (sft_window_update(window))
     {
@@ -37,19 +39,30 @@ int main()
             window = NULL;
         }
         if (sft_input_keyReleased(sft_key_Capslock))
+        {
             sft_window_setFlag(window, sft_flag_hidden, 0);
+            hidden = true;
+        }
         if (sft_input_keyPressed(sft_key_Capslock))
+        {
             sft_window_setFlag(window, 0, sft_flag_hidden);
+            hidden = false;
+        }
+        if (sft_input_keyReleased(sft_key_R) && sft_input_keyState(sft_key_Control))
+            sft_screenshot(screen);
 
-        sft_window_drawImage(window, screen,
-            0, 0, screen->width, screen->height, 0, 0);
+        if (!hidden)
+        {
+            sft_window_drawImage(window, screen,
+                0, 0, screen->width, screen->height, 0, 0);
 
-        sft_window_drawRect(window,
-            sft_input_mousePos(window).x - 50,
-            sft_input_mousePos(window).y - 50,
-            100, 100, 0);
+            sft_window_drawRect(window,
+                sft_input_mousePos(window).x - 50,
+                sft_input_mousePos(window).y - 50,
+                100, 100, 0);
 
-        sft_window_display(window);
+            sft_window_display(window);
+        }
     }
     sft_window_close(window);
     sft_image_delete(screen);
--- softdraw/input/input.c
+++ softdraw/input/input.c
@@ -21,41 +21,57 @@ void sft_input_updateInput()
 
 bool sft_input_keyState(sft_key key)
 {
+	if (key >= sft_key_Count)
+		return false;
 	return sft_input_keys[key] & 1;
 }
 
 bool sft_input_keyReleased(sft_key key)
 {
+	if (key >= sft_key_Count)
+		return false;
 	return !sft_input_keyState(key) && sft_input_keyLast(key);
 }
 
 bool sft_input_keyPressed(sft_key key)
 {
+	if (key >= sft_key_Count)
+		return false;
 	return sft_input_keyState(key) && !sft_input_keyLast(key);
 }
 
 bool sft_input_keyLast(sft_key key)
 {
+	if (key >= sft_key_Count)
+		return false;
 	return (sft_input_keys[key] >> 1) & 1;
 }
 
 
 bool sft_input_clickState(sft_key button)
 {
+	if (button >= sft_click_Count)
+		return false;
 	return sft_input_clicks[button] & 1;
 }
 
 bool sft_input_clickReleased(sft_key button)
 {
+	if (button >= sft_click_Count)
+		return false;
 	return !sft_input_clickState(button) && sft_input_clickLast(button);
 }
 
 bool sft_input_clickPressed(sft_key button)
 {
+	if (button >= sft_click_Count)
+		return false;
 	return sft_input_clickState(button) && !sft_input_clickLast(button);
 }
 
 bool sft_input_clickLast(sft_key button)
 {
+	if (button >= sft_click_Count)
+		return false;
 	return (sft_input_clicks[button] >> 1) & 1;
 }
